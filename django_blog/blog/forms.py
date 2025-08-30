79854f809a8e73c849103ef9f7903fded2f2d97e
Daniel <danielwael930@gmail.com>
2025-08-30 11:21:29 +0300

    Add required permissions import and generic views for Book CRUD

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Post
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']  # include tags
+        widgets = {
+            'tags': TagWidget(),  # <-- add this line
+        }
