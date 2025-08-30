ac55cc147dd34e70c6aa85570ec9406416323ec0
Daniel <danielwael930@gmail.com>
2025-08-30 07:18:33 +0300

    Move templates into relationship_app folder to match app-relative paths

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import DetailView
 from .models import Library
+
 # --- Function-based view ---
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # Use full app-relative template path
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 # --- Class-based view ---
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # Use full app-relative path
     context_object_name = 'library'
