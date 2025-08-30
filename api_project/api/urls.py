bc541b6c3b7ea21f9c834c5744504228c493c8f7
Daniel <danielwael930@gmail.com>
2025-08-30 08:48:13 +0300

    Import ExampleForm in views.py and use it in add/edit book views

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import BookList
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-        path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # include the api app urls
 ]
