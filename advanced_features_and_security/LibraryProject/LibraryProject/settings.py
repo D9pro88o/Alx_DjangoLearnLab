4e41fe91fe3beee79cbb0421d535d65a320a4539
Daniel <danielwael930@gmail.com>
2025-08-30 08:29:23 +0300

    Import ExampleForm in views.py and use it in add/edit book views

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -101,6 +101,9 @@ SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+# If your app is behind a proxy (like Nginx) that handles HTTPS
+# This tells Django to trust the X-Forwarded-Proto header to detect HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Secure cookies
 SESSION_COOKIE_SECURE = True
