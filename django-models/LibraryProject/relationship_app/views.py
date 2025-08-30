063e368cac0ccc5517547e72f328761f10b1c08b
Daniel <danielwael930@gmail.com>
2025-08-30 07:25:47 +0300

    Add Library import and correct DetailView import in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book, Library
+# relationship_app/views.py
 
-# Function-based view
+from django.shortcuts import render
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
