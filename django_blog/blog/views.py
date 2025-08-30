d7baed229023419027f20ecc59653a5046c0e560
Daniel <danielwael930@gmail.com>
2025-08-30 11:01:19 +0300

    Add required permissions import and generic views for Book CRUD

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
-from .forms import RegisterForm
+from django.shortcuts import render, redirect
 
-def register_view(request):
+def login_view(request):
     if request.method == "POST":
-        form = RegisterForm(request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.get_user()
             login(request, user)
             return redirect("home")  # Replace "home" with your homepage URL name
     else:
-        form = RegisterForm()
-    return render(request, "blog/register.html", {"form": form})
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
