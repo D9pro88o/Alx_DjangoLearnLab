17beeb8e33321c8639d5ff8eeef6dca589f0256b
Daniel <danielwael930@gmail.com>
2025-08-31 22:01:48 +0300

    Push The project Form the local repo

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+# social_media_api/urls.py
+from django.urls import include
 
 urlpatterns = [
+    path('api/', include('posts.urls')),  # posts and comments API
+    # other urls...
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view()),  # <- make sure this line exists
 ]
