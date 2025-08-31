342337fa08b8885fcf47835e5fd9d9861ce75a60
Daniel <danielwael930@gmail.com>
2025-08-31 21:35:09 +0300

    Push The project Form the local repo

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,35 +1,17 @@
-# accounts/serializers.py
-
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
-        read_only_fields = ['id']
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'password2']
-
-    def validate(self, attrs):
-        if attrs.get('password') != attrs.get('password2'):
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        username = validated_data.get('username')
-        email = validated_data.get('email', '')
-        password = validated_data.get('password')
-
-        # create_user will handle password hashing
-        user = User.objects.create_user(username=username, email=email, password=password)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)
         return user
