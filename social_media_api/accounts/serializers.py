014651b5a1c07eccab1ff4779c5fe8d6f492252d
Daniel <danielwael930@gmail.com>
2025-08-31 21:47:23 +0300

    Push The project Form the local repo

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,13 +4,18 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.CharField(required=False)
+    password = serializers.CharField(write_only=True, min_length=8)
 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
     def create(self, validated_data):
-        # Create the user normally without manually creating a token
-        user = User.objects.create(**validated_data)
+        # Use create_user to handle password hashing
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        # Create an auth token for the new user
+        Token.objects.create(user=user)
         return user
